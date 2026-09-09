e31d341c1cb883bc1fcb70c777f288899c76a388
Your Name <your@email.com>
2026-09-10 02:05:36 +0530

    MongoDB connection successful

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.log('MongoDB connection error:', err));
 
 app.get('/test', (req, res) => {
   res.send('Server is working!');
